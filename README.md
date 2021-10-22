5b78bfd44257159b65a55f5312cb4e35eaf8d357
Lydia <l.king18@nuigalway.ie>
2021-10-22 16:24:20 +0100

    Update

---
+++ README.md
@@ -0,0 +1,12 @@
+## GEOXAR: An R Shiny app supporting cancer genomics survival analysis with cBioPortal
+
+We have developed GEOXAR (Genomics Explorer using Survival Analysis in R), an R Shiny App, incorporating a range of R packages enabling users 
+to efficiently explore and visualise clinical and genomic data obtained from cBioPortal. GEOXAR uses an intuitive GUI and multiple tab panels supporting a 
+range of functionalities including data upload and initial exploration, data recoding and subsetting, multiple visualisations, survival analysis, 
+statistical analysis and mutation analysis, in addition to facilitating reproducible research.
+
+Available at: https://lydiaking.shinyapps.io/GEOXAR/
+
+Also can be run using: ```shiny::runGitHub(repo = 'GEOXAR', username = 'Lydia-King', ref="main")```
+
+
---
+++ Setup.R
@@ -0,0 +1,122 @@
+## Install and load up libraries 
+# Shiny Dashboard setup
+
+if (!requireNamespace("shiny", quietly = TRUE))
+  BiocManager::install("shiny")
+if (!requireNamespace("shinymeta", quietly = TRUE))
+  BiocManager::install("shinymeta")
+if (!requireNamespace("shinydashboard", quietly = TRUE))
+  BiocManager::install("shinydashboard")
+if (!requireNamespace("dashboardthemes", quietly = TRUE))
+  BiocManager::install("dashboardthemes")
+if (!requireNamespace("shinydashboardPlus", quietly = TRUE))
+  BiocManager::install("shinydashboardPlus")
+if (!requireNamespace("shinyWidgets", quietly = TRUE))
+  BiocManager::install("shinyWidgets")
+if (!requireNamespace("shinycssloaders", quietly = TRUE))
+  BiocManager::install("shinycssloaders")
+if (!requireNamespace("shinylogs", quietly = TRUE))
+  BiocManager::install("shinylogs")
+if (!requireNamespace("fontawesome", quietly = TRUE))
+  BiocManager::install("fontawesome")
+
+library(shiny) # Build shiny app
+library(shinymeta) # R Script
+library(shinydashboard) # For shiny dashboard layout
+library(dashboardthemes) # For shiny dashboard layout
+library(shinydashboardPlus) # For shiny dashboard layout
+library(shinyWidgets) # For shiny dashboard layout 
+library(shinycssloaders) # For loading spinners
+library(shinylogs) # For input Log
+library(fontawesome) # For icons
+
+# Data Tables and Formatting of Data
+if (!requireNamespace("DT", quietly = TRUE))
+  BiocManager::install("DT")
+if (!requireNamespace("tidyverse", quietly = TRUE))
+  BiocManager::install("tidyverse")
+if (!requireNamespace("fabricatr", quietly = TRUE))
+  BiocManager::install("fabricatr")
+if (!requireNamespace("reshape2", quietly = TRUE))
+  BiocManager::install("reshape2")
+if (!requireNamespace("operator.tools", quietly = TRUE))
+  BiocManager::install("operator.tools")
+
+library(DT) # Used to create scrollable datatables 
+library(tidyverse) # manipulate and select and stringr, ggplot2
+library(fabricatr) # function split_quantile
+library(reshape2) # Melt function
+library(operator.tools) # not in
+
+# Figures (Exploratory plots, survival KM curves and Survival Trees)
+if (!requireNamespace("RColorBrewer", quietly = TRUE))
+  BiocManager::install("RColorBrewer")
+if (!requireNamespace("rpart", quietly = TRUE))
+  BiocManager::install("rpart")
+if (!requireNamespace("rpart.plot", quietly = TRUE))
+  BiocManager::install("rpart.plot")
+if (!requireNamespace("partykit", quietly = TRUE))
+  BiocManager::install("partykit")
+if (!requireNamespace("survival", quietly = TRUE))
+  BiocManager::install("survival")
+if (!requireNamespace("survminer", quietly = TRUE))
+  BiocManager::install("survminer")
+
+library(RColorBrewer) # Color palette 
+library(rpart) # Survival trees (rpart)
+library(rpart.plot) # Survival trees (rpart)
+library(partykit) # Survival trees (Ctree)
+library(survival) # Survival analysis
+library(survminer) # Survival analysis
+
+# Association/Stat test 
+if (!requireNamespace("stats", quietly = TRUE))
+  BiocManager::install("stats")
+if (!requireNamespace("rstatix", quietly = TRUE))
+  BiocManager::install("rstatix")
+if (!requireNamespace("DescTools", quietly = TRUE))
+  BiocManager::install("DescTools")
+if (!requireNamespace("car", quietly = TRUE))
+  BiocManager::install("car")
+
+library(stats)
+library(rstatix)
+library(DescTools) # Dunns Test
+library(car) # leveneTest
+
+# maftools
+if (!requireNamespace("R.utils", quietly = TRUE))
+  BiocManager::install("R.utils")
+library(R.utils)
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  BiocManager::install("BiocManager")
+
+options(repos = BiocManager::repositories())
+library(BiocManager)
+if (!requireNamespace("maftools", quietly = TRUE))
+  BiocManager::install("maftools")
+library(maftools)
+
+validate <- shiny::validate
+
+## Allow large file uploads, set spinner up and sanitize errors
+options(shiny.maxRequestSize=30*1024^3) 
+options(spinner.size=1)
+options(spinner.type = 8)
+options(spinner.color="#012B45")
+
+## Functions 
+# Function to get complete cases
+completeFun <- function(data, desiredCols) {
+  completeVec <- complete.cases(data[, desiredCols])
+  return(data[completeVec, ])
+}
+
+# Get Mode 
+getmode <- function(v) {uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]}
+
+# experiment with the multiplier to find the perfect position
+give.n <- function(x){return(c(y = median(x)*1.05, label = length(x)))}   
+
