9f3d841564dc552af57195ac19af8f22daaa4e3d
Lydia <l.king18@nuigalway.ie>
2021-12-18 10:22:05 +0000

    License

--- Setup.R
+++ Setup.R
@@ -1,3 +1,24 @@
+## MIT License
+# Copyright (c) 2021 Lydia King
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#   
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 ## Install and load up libraries 
 # Shiny Dashboard setup
 
@@ -85,11 +106,13 @@ if (!requireNamespace("coin", quietly = TRUE))
 library(RColorBrewer) # Color palette
 
 # maftools
+library(BiocManager)
 options(repos = BiocManager::repositories())
 if (!requireNamespace("maftools", quietly = TRUE))
   BiocManager::install("maftools")
 library(maftools)
 
+options("repos")
 
 validate <- shiny::validate
 
